c7524daf87c2a22962a1cb2cc099791360d50a49
Cartmanishere <apbetahouse45@gmail.com>
2018-10-07 16:47:16 +0530

    Updates

---
+++ precache-manifest.a41315a77db17a19736a949773183ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5beae74021d7e8521c2a",
+    "url": "/polynomial_regression/static/js/runtime~main.5beae740.js"
+  },
+  {
+    "revision": "9143b1a0b3b491da82ba",
+    "url": "/polynomial_regression/static/js/main.9143b1a0.chunk.js"
+  },
+  {
+    "revision": "7c0faff2cbdb387a776e",
+    "url": "/polynomial_regression/static/js/1.7c0faff2.chunk.js"
+  },
+  {
+    "revision": "9143b1a0b3b491da82ba",
+    "url": "/polynomial_regression/static/css/main.56a26e7d.chunk.css"
+  },
+  {
+    "revision": "c1860b4ac2f33d90f347b7e58699a585",
+    "url": "/polynomial_regression/index.html"
+  }
+];
